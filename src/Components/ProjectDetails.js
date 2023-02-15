c4961f5aa3c00e8f296b5aab874a049998d4003b
MarsiaRochelin <marsrochelin@pursuit.org>
2023-02-15 18:11:14 -0500

    Show page inital build

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
 
 const ProjectDetails = () => {
-  return <div></div>;
+  const [project, setProject] = useState({});
+  const {
+    project_name,
+    project_description,
+    technology_utilize,
+    start_date,
+    due_date,
+    email,
+  } = project;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/projects/${id}`)
+      .then((res) => setProject(res.data))
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  return (
+    <div className="Show">
+      <p>{project_name}</p>
+      <p>{project_description}</p>
+      <p>{technology_utilize}</p>
+      <p>{start_date}</p>
+      <p>{due_date}</p>
+      <p>{email}</p>
+      <div className="Buttons">
+        <Link>
+          <button>Back</button>
+        </Link>
+        <Link>
+          <button>Edit</button>
+        </Link>
+        <Link>
+          <button>Delete</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetails;
